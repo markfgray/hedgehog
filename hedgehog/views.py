223bf434d49fd2146ac20e85d5dc46037a32e646
Mark Gray <mark.gray@hedgehog.fyi>
2019-05-06 19:30:00 +0100

    added login and signup pages

--- hedgehog/views.py
+++ hedgehog/views.py
@@ -24,3 +24,31 @@ def signup():
 @app.route('/thanks', methods=["GET"])
 def thanks():
 	return render_template('thankyou.html')
+
+@app.route('/search', methods=["GET", "POST"])
+def search():
+	return render_template('search.html')
+
+@app.route('/leavereview', methods=["GET", "POST"])
+def leaveReview():
+	return render_template('leavereview.html')
+
+@app.route('/localplaces', methods=["GET", "POST"])
+def localPlaces():
+	return render_template('localplaces.html')
+
+@app.route('/placedetails', methods=["GET", "POST"])
+def placeDetails():
+	return render_template('placedetails.html')
+
+@app.route('/results', methods=["GET", "POST"])
+def results():
+	return render_template('results.html')
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+	return render_template('signup.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+	return render_template('login.html')
